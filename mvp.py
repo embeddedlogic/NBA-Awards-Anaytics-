a91f05f69367c9e30824e98a0607128187a4fb6e
Nehemiah Ezan <nehemiahezan@gmail.com>
2026-05-09 19:03:33 -0400

    advanced stats setup for MVP

--- mvp.py
+++ mvp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from nba_api.stats.endpoints import  leaguedashplayerstats
 from nba_api.stats.endpoints import  leaguedashplayerclutch
+from nba_api.stats.endpoints import playerdashboardbyonoff
 
 
 def mvp():
@@ -20,6 +21,15 @@ def mvp():
      point_diff=5, # score difference must be 5 pts or less 
      ahead_behind="Ahead or Behind" # cluch stats from if team is winning or losing 
     )
+     player_on_off = playerdashboardbyonoff(
+     season="2025-26", 
+     season_type_playoffs="Regular Season",
+     per_mode_detailed="PerGame",
+     measure_type_detailed="Advanced", # advanced analtyics stats
+     pace_adjust="N", # dont adjust stats based on team pace 
+     plus_minus="N", # dont include plus/minus calculations 
+     rank="N", # dont add ranking colums 
+     )
      mvp = stats.get_data_frames()[0]
      mvp_table = mvp[[
             "PLAYER_NAME",
